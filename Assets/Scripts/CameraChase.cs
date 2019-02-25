06e71b6ea3fc4e14521e143349c8a7495cc18e96
Yuki Sakai <yuukisakai123@gmail.com>
2019-02-25 22:14:50 +0900

    no message

---
+++ Assets/Scripts/CameraChase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChase : MonoBehaviour
+{
+    public GameObject chaseObject;
+    Vector3 difVector;
+    public float difPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difVector = chaseObject.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = chaseObject.transform.position - difVector;
+        this.transform.position = new Vector3(this.transform.position.x, difPosY, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/CharacterRun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRun : MonoBehaviour
+{
+    Rigidbody rb;
+    public float runSpeed;
+    Vector3 speedVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedVector = new Vector3(0, 0, runSpeed);
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = speedVector;
+    }
+}
